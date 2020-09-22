ebfef6bb005fc12aacf3bd2c7499d9ec933a9708
HongfeiC <hochen1998@gmail.com>
2020-09-22 00:46:18 -0700

    hc3222 completed

--- server.py
+++ server.py
@@ -3,38 +3,43 @@ from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
 
+# added a new key "rank" to indicate ordering
 scoreboard = [
     {
     "id": 1,
     "name": "Boston Bruins",
-    "score": 7
+    "score": 7,
+    "rank": 1
     },
 
     {
     "id": 2,
     "name": "Tampa Bay Lightning", 
-    "score": 5
+    "score": 5,
+    "rank": 2
     },
 
     {
     "id": 3,
     "name": "Toronto Maple Leafs", 
-    "score": 2
+    "score": 2,
+    "rank": 3
     },
 
     {
     "id": 4,
     "name": "Florida Panthers", 
-    "score": 1
+    "score": 1,
+    "rank": 4
     },
 
     {
     "id": 5,
     "name": "Buffalo Sabres", 
-    "score": 1
+    "score": 1,
+    "rank": 5
     },
 ]
-
 
 @app.route('/')
 def show_scoreboard():
@@ -53,6 +58,19 @@ def increase_score():
 
     return jsonify(scoreboard=scoreboard)
 
+@app.route('/swap_order', methods=['GET', 'POST'])
+def swap_order():
+    global scoreboard
+    json_data = request.get_json()
+
+    # update the ranks of two teams
+    for team_num in ["team1", "team2"]:
+        team_id = json_data[team_num]["id"]
+        for team in scoreboard:
+            if team["id"] == team_id:
+                team["rank"] = json_data[team_num]["rank"]
+
+    return jsonify(scoreboard=scoreboard)
 
 if __name__ == '__main__':
    app.run(debug = True)
--- static/scoreboard.js
+++ static/scoreboard.js
@@ -1,8 +1,22 @@
+// maps to store latest scoreboard info
+let rank_team_map = new Map();
+let id_team_map = new Map();
+
 function display_scoreboard(scoreboard){
+  // initialize maps
+  rank_team_map = new Map();
+  id_team_map = new Map();
   $("#teams").empty();
   $.each(scoreboard, function(index, team){
-    addTeamView(team.id, team.name, team.score);
+    // populate the maps
+    rank_team_map.set(team.rank, team);
+    id_team_map.set(team.id, team);
   });
+  // add team by ranking order
+  for (let r = 1; r <= rank_team_map.size; r++){
+    const team = rank_team_map.get(r);
+    addTeamView(team.id, team.name, team.score);
+  }
 }
 
 function addTeamView(id, name, score){
@@ -12,7 +26,7 @@ function addTeamView(id, name, score){
   var button_template = $("<div class = col-md-2></div>");
   var increase_button = $("<button class = increase-button>+</button>");
   $(increase_button).click(function(){
-    increase_score(id);
+    increase_score(id, score_template);
   });
   name_template.text(name);
   score_template.text(score);
@@ -23,8 +37,22 @@ function addTeamView(id, name, score){
   $("#teams").append(team_template);
 }
 
-function increase_score(id){
-  var team_id = {"id": id}
+function increase_score(id, score_template){
+  var team_id = {"id": id};
+
+  // update score on the page
+  const newScore = parseInt(score_template.text()) + 1;
+  score_template.text(newScore);
+
+  // update in maps
+  const team = id_team_map.get(id);
+  team["score"] = newScore;
+  const rank = team.rank;
+  rank_team_map.set(rank, team);
+
+  // rearrange the order if needed
+  sort_team(rank, newScore);
+
   $.ajax({
     type: "POST",
     url: "increase_score",                
@@ -41,6 +69,49 @@ function increase_score(id){
         console.log(error)
     }
   });
+}
+
+// rearrange the order of the teams
+function sort_team(rank, score){
+  // get the team which ranks one higher
+  if (rank > 1){
+    const team = rank_team_map.get(rank);
+    const prev_team = rank_team_map.get(rank - 1);
+    const prev_score = prev_team.score;
+    if (prev_score < score){
+      const team_els = $("#teams").children(".row");
+      // swap the two teams
+      team_els[rank - 1].parentNode.insertBefore(team_els[rank - 1], team_els[rank - 2]);
+      // update maps
+      team["rank"] = rank - 1;
+      prev_team["rank"] = rank;
+      rank_team_map.set(rank, prev_team);
+      rank_team_map.set(rank-1, team);
+      id_team_map.set(team.id, team);
+      id_team_map.set(prev_team.id, prev_team);
+
+      const post_data = {"team1": {"id": team.id, "rank": team.rank},
+                          "team2": {"id": prev_team.id, "rank": prev_team.rank}};
+
+      // post to server
+      $.ajax({
+        type: "POST",
+        url: "swap_order",
+        dataType : "json",
+        contentType: "application/json; charset=utf-8",
+        data : JSON.stringify(post_data),
+        success: function(result){
+
+        },
+        error: function(request, status, error){
+            console.log("Error");
+            console.log(request)
+            console.log(status)
+            console.log(error)
+        }
+      });
+    }
+  }
 }
 
 $(document).ready(function(){
