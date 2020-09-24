a2a80cb122a74c4e4e8b78469c33c6d0832ba1d8
Mina Wang <shijie.wang@columbia.edu>
2020-09-24 03:01:37 -0400

    Reorders the list and makes the scores update without refreshing

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -10,7 +10,9 @@ function addTeamView(id, name, score) {
   var name_template = $("<div class = col-md-5></div>");
   var score_template = $("<div id='" + id + "'class = col-md-2></div>");
   var button_template = $("<div class = col-md-2></div>");
-  var increase_button = $("<button class=increase-button>+</button>");
+  var increase_button = $(
+    "<button id='" + id + "'class=increase-button>+</button>"
+  );
   $(increase_button).click(function () {
     increase_score(id);
   });
@@ -23,7 +25,22 @@ function addTeamView(id, name, score) {
   $("#teams").append(team_template);
 }
 
-function change_order() {}
+function compare(a, b) {
+  const scoreA = a.score;
+  const scoreB = b.score;
+  let comparison = 0;
+  if (scoreA > scoreB) {
+    comparison = 1;
+  } else if (scoreA < scoreB) {
+    comparison = -1;
+  }
+  return comparison * -1;
+}
+
+function change_order(scoreboard) {
+  scoreboard.sort(compare);
+  display_scoreboard(scoreboard);
+}
 
 function increase_score(id) {
   var team_id = { id: id };
@@ -42,6 +59,11 @@ function increase_score(id) {
         }
       }
       $("div#" + id).text(theObj.score);
+      $("#teams").empty();
+      $.each(result, function (index, team) {
+        addTeamView(team.id, team.name, team.score);
+      });
+      console.log(result);
     },
     error: function (request, status, error) {
       console.log("Error");
