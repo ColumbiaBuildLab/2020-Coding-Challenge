f487f6c99b3becdbe5a02fb685c755d6075d4404
jhw2167 <jhw2167@columbia.edu>
2020-09-25 20:38:20 -0700

    Made necessary changes, ready to push

--- server.py
+++ server.py
@@ -42,14 +42,27 @@ def show_scoreboard():
 
 @app.route('/increase_score', methods=['GET', 'POST'])
 def increase_score():
+
+    #This function is called via flask-js-html interaction
+    #each time an "add" button is pressed on the homescreen for 
+    # a particular team, we can identify this team with the
+    #team_id value to determine who's score to increase
+
+
+    #tells us the scoreboard var we are using here is global
     global scoreboard
 
     json_data = request.get_json()   
-    team_id = json_data["id"]  
+    team_id = json_data["id"]
     
+    #for each team in scoreboard, if the team was apart of the 
+    #json request data, increase its value
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
+
+    #sort the list by score
+    scoreboard = sorted(scoreboard, key=lambda k: k['score'], reverse=True)
 
     return jsonify(scoreboard=scoreboard)
 
--- static/scoreboard.js
+++ static/scoreboard.js
@@ -32,7 +32,9 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+        //We update the scoreboard with the *new* values, by
+		//calling display scoreboard again
+		display_scoreboard(result.scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
