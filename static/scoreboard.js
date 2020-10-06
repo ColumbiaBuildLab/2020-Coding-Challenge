4d85129587d75941d283959c6dafa3d37bf7cfd3
kolber <>
2020-10-06 00:00:24 -0400

    final submission

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -1,6 +1,7 @@
 function display_scoreboard(scoreboard){
   $("#teams").empty();
   $.each(scoreboard, function(index, team){
+    console.log(index, team);
     addTeamView(team.id, team.name, team.score);
   });
 }
@@ -27,18 +28,22 @@ function increase_score(id){
   var team_id = {"id": id}
   $.ajax({
     type: "POST",
-    url: "increase_score",                
+    url: "increase_score",
     dataType : "json",
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+        console.log(result);
+        display_scoreboard(result.scoreboard);
+
     },
+
     error: function(request, status, error){
         console.log("Error");
         console.log(request)
         console.log(status)
         console.log(error)
+
     }
   });
 }
