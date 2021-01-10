e7b58e32763a7acb9a975c835cd0d859660dcae2
adibsad <sadadib1@users.noreply.github.com>
2021-01-10 17:30:44 -0500

    Sad Adib (sa3783) Coding Challenge Submission

--- server.py
+++ server.py
@@ -51,8 +51,43 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    # Custom quicksort 
+    # Alternatively we could also use scoreboard.sort() with score as the key
+    my_quicksort(scoreboard, 0, len(scoreboard) - 1)
+
     return jsonify(scoreboard=scoreboard)
 
+def my_quicksort(scoreboard, start, end):
+    def partition(scoreboard, i, j):
+        pivot = scoreboard[j]["score"]
+        p_idx = j
+
+        j -= 1
+        while True:
+            while not i > j and scoreboard[j]["score"] <= pivot:
+                j -= 1
+            
+            while not i > j and scoreboard[i]["score"] >= pivot:
+                i += 1
+            
+            if not i > j:
+                temp = scoreboard[i]
+                scoreboard[i] = scoreboard[j]
+                scoreboard[j] = temp
+            else:
+                break
+        
+        temp = scoreboard[i]
+        scoreboard[i] = scoreboard[p_idx]
+        scoreboard[p_idx] = temp
+
+        return i
+
+    if start < end:
+        idx = partition(scoreboard, start, end)
+
+        my_quicksort(scoreboard, start, idx - 1)
+        my_quicksort(scoreboard, idx + 1, end)
 
 if __name__ == '__main__':
    app.run(debug = True)
