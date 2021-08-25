75222de4199025bd77c830adbea2c9cf32cbfab5
gauravsinha7 <gauravsinha952@gmail.com>
2021-08-25 21:09:13 +0530

    Score refresh dynamic and sort

--- server.py
+++ server.py
@@ -1,37 +1,37 @@
 from flask import Flask
 from flask import render_template
-from flask import Response, request, jsonify
+from flask import request, jsonify
 app = Flask(__name__)
 
 scoreboard = [
     {
-    "id": 1,
-    "name": "Boston Bruins",
-    "score": 7
+        "id": 1,
+        "name": "Boston Bruins",
+        "score": 7
     },
 
     {
-    "id": 2,
-    "name": "Tampa Bay Lightning", 
-    "score": 5
+        "id": 2,
+        "name": "Tampa Bay Lightning",
+        "score": 5
     },
 
     {
-    "id": 3,
-    "name": "Toronto Maple Leafs", 
-    "score": 2
+        "id": 3,
+        "name": "Toronto Maple Leafs",
+        "score": 2
     },
 
     {
-    "id": 4,
-    "name": "Florida Panthers", 
-    "score": 1
+        "id": 4,
+        "name": "Florida Panthers",
+        "score": 1
     },
 
     {
-    "id": 5,
-    "name": "Buffalo Sabres", 
-    "score": 1
+        "id": 5,
+        "name": "Buffalo Sabres",
+        "score": 1
     },
 ]
 
@@ -39,6 +39,7 @@ scoreboard = [
 @app.route('/')
 def show_scoreboard():
     return render_template('scoreboard.html', scoreboard = scoreboard) 
+
 
 @app.route('/increase_score', methods=['GET', 'POST'])
 def increase_score():
@@ -50,7 +51,7 @@ def increase_score():
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
-
+    scoreboard = sorted(scoreboard, key=lambda team_data: team_data["score"], reverse=True)
     return jsonify(scoreboard=scoreboard)
 
 
