575942a18bab3bbbb50529e31a6a79ebc4e8e97c
ellenzhao318 <mochadafu@126.com>
2021-08-25 23:29:22 -0400

    CBL challange
    Xinxin Zhao
    xz3061@columbia.edu

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -13,6 +13,8 @@ function addTeamView(id, name, score){
   var increase_button = $("<button class = increase-button>+</button>");
   $(increase_button).click(function(){
     increase_score(id);
+    location.reload();
+    window.parent.location = window.parent.location.href;
   });
   name_template.text(name);
   score_template.text(score);
