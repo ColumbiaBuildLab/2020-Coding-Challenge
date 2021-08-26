133c70abdde25af5268cab4b4b6319d0380e6cda
BenjaminJenney <benjenns@gmail.com>
2021-08-26 19:29:47 -0400

    dynamic update, sort, and team logos
    scores update dynamically, sorted in descending (non-increasing) order, and added team logos for some pizazz.

--- server.py
+++ server.py
@@ -1,37 +1,39 @@
 from flask import Flask
 from flask import render_template
 from flask import Response, request, jsonify
+import os
+
 app = Flask(__name__)
 
 scoreboard = [
     {
-    "id": 1,
-    "name": "Boston Bruins",
-    "score": 7
+        "id": 1,
+        "name": "Boston Bruins",
+        "score": 7
     },
 
     {
-    "id": 2,
-    "name": "Tampa Bay Lightning", 
-    "score": 5
+        "id": 2,
+        "name": "Tampa Bay Lightning", 
+        "score": 5
     },
 
     {
-    "id": 3,
-    "name": "Toronto Maple Leafs", 
-    "score": 2
+        "id": 3,
+        "name": "Toronto Maple Leafs", 
+        "score": 2
     },
 
     {
-    "id": 4,
-    "name": "Florida Panthers", 
-    "score": 1
+        "id": 4,
+        "name": "Florida Panthers", 
+        "score": 1
     },
 
     {
-    "id": 5,
-    "name": "Buffalo Sabres", 
-    "score": 1
+        "id": 5,
+        "name": "Buffalo Sabres", 
+        "score": 1
     },
 ]
 
@@ -46,13 +48,14 @@ def increase_score():
 
     json_data = request.get_json()   
     team_id = json_data["id"]  
-    
+
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
 
-    return jsonify(scoreboard=scoreboard)
+    scoreboard = sorted(scoreboard, key=lambda k: k['score'], reverse=True) 
 
+    return jsonify(scoreboard=scoreboard)
 
 if __name__ == '__main__':
    app.run(debug = True)
--- static/scoreboard.js
+++ static/scoreboard.js
@@ -1,19 +1,22 @@
 function display_scoreboard(scoreboard){
   $("#teams").empty();
+  sort_scores(scoreboard);  
   $.each(scoreboard, function(index, team){
     addTeamView(team.id, team.name, team.score);
   });
 }
 
 function addTeamView(id, name, score){
-  var team_template = $("<div class = row></div>");
-  var name_template = $("<div class = col-md-5></div>");
-  var score_template = $("<div class = col-md-2></div>");
+  var team_template   = $("<div class = row></div>");
+  var image_template  = $("<img class = col-xs-1 src=/static/team_images/" + name.substring(0, name.indexOf(' ')) + ".png></img>");
+  var name_template   = $("<div class = col-md-5></div>");
+  var score_template  = $("<div class = col-md-2></div>");
   var button_template = $("<div class = col-md-2></div>");
   var increase_button = $("<button class = increase-button>+</button>");
   $(increase_button).click(function(){
     increase_score(id);
   });
+  team_template.append(image_template);
   name_template.text(name);
   score_template.text(score);
   button_template.append(increase_button);
@@ -32,7 +35,7 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+       display_scoreboard(result.scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
@@ -41,6 +44,12 @@ function increase_score(id){
         console.log(error)
     }
   });
+}
+
+function sort_scores(scoreboard){
+  [scoreboard].sort(function(team1, team2){
+    return team1.score - team2.score;
+  })
 }
 
 $(document).ready(function(){
