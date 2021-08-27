3591bea1041345ab59a2ffa33f2ca8d1d463b9f6
Pranav H. Kajgaonkar <pranav.kajgaonkar@gmail.com>
2021-08-27 02:44:16 -0400

    (08/26/2021) Added dynamic and reverse sorted scoreboard

--- server.py
+++ server.py
@@ -38,10 +38,25 @@ scoreboard = [
 
 @app.route('/')
 def show_scoreboard():
+    ''' 
+    -> This function will display the Scoreboard whenever the page is loaded.
+    -> Scoreboard is a 'List' or 'Array'. Hence, the fastest way to sort it is use the in-built Sort function.
+    -> This function has a key parameter to specify a function (or other callable) to be called on each list 
+       element prior to making comparisons. We can also use a custom function/method here.
+    -> This method also accepts a parameter called 'reverse' to sort the list in decreasing order.  
+    '''
+    scoreboard.sort(key=lambda x: x["score"],reverse=True)
     return render_template('scoreboard.html', scoreboard = scoreboard) 
 
 @app.route('/increase_score', methods=['GET', 'POST'])
 def increase_score():
+    ''' 
+    -> This function will increase the score for the corrosponding team whenever the button right next to it is pressed.
+    -> We are parsing the incoming JSON request data and returing it using the get_json method.
+    -> For whichever id(s) the button was pressed, the score will be incremented.
+    
+    -> We will use the same sorting technique as the previous function.
+    '''
     global scoreboard
 
     json_data = request.get_json()   
@@ -51,12 +66,9 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    scoreboard.sort(key=lambda x: x["score"],reverse=True)
     return jsonify(scoreboard=scoreboard)
 
 
 if __name__ == '__main__':
    app.run(debug = True)
-
-
-
-
