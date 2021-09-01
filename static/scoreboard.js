8cbafff58a893a5ba05a6be3a911386c5aa1ab30
mrm2234 <60762357+mrm2234@users.noreply.github.com>
2021-09-01 04:24:25 -0400

    Update scoreboard.js

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -32,7 +32,9 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+      console.log("load")
+      location.reload()
+      console.log("done loading")
     },
     error: function(request, status, error){
         console.log("Error");
