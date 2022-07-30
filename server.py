8714962852fe8480b26fb146ded2c3b1e71a1b3e
Aisha Mann <32098514+AishaMann21@users.noreply.github.com>
2022-07-30 18:40:18 -0400

    added immediate display and sorting functionality

--- server.py
+++ server.py
@@ -5,58 +5,55 @@ app = Flask(__name__)
 
 scoreboard = [
     {
-    "id": 1,
-    "name": "Boston Bruins",
-    "score": 7
+        "id": 1,
+        "name": "Boston Bruins",
+        "score": 7
     },
 
     {
-    "id": 2,
-    "name": "Tampa Bay Lightning", 
-    "score": 5
+        "id": 2,
+        "name": "Tampa Bay Lightning",
+        "score": 5
     },
 
     {
-    "id": 3,
-    "name": "Toronto Maple Leafs", 
-    "score": 2
+        "id": 3,
+        "name": "Toronto Maple Leafs",
+        "score": 2
     },
 
     {
-    "id": 4,
-    "name": "Florida Panthers", 
-    "score": 1
+        "id": 4,
+        "name": "Florida Panthers",
+        "score": 1
     },
 
     {
-    "id": 5,
-    "name": "Buffalo Sabres", 
-    "score": 1
+        "id": 5,
+        "name": "Buffalo Sabres",
+        "score": 1
     },
 ]
 
 
 @app.route('/')
 def show_scoreboard():
-    return render_template('scoreboard.html', scoreboard = scoreboard) 
+    return render_template('scoreboard.html', scoreboard=scoreboard)
+
 
 @app.route('/increase_score', methods=['GET', 'POST'])
 def increase_score():
     global scoreboard
 
-    json_data = request.get_json()   
-    team_id = json_data["id"]  
-    
+    json_data = request.get_json()
+    team_id = json_data["id"]
+
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
 
-    return jsonify(scoreboard=scoreboard)
+    return jsonify(scoreboard=sorted(scoreboard, key=lambda item: item['score'], reverse=True))
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
-
-
-
-
+    app.run(debug=True)
