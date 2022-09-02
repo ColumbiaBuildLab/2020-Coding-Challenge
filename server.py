141a1e387450f3afa2981a6297cee905df9fd3ab
Shashank Nathani <sn2954@columbia.edu>
2022-09-01 22:50:19 -0400

    Refresh screen on score change and sort scores

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+# Shashank Nathani
+# sn2954
+
 from flask import Flask
 from flask import render_template
 from flask import Response, request, jsonify
@@ -38,6 +41,7 @@ scoreboard = [
 
 @app.route('/')
 def show_scoreboard():
+    global scoreboard
     return render_template('scoreboard.html', scoreboard = scoreboard) 
 
 @app.route('/increase_score', methods=['GET', 'POST'])
@@ -50,6 +54,11 @@ def increase_score():
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
+
+    # sort in decreasing order based on 'score'
+    # Shashank Nathani
+    # UNI: sn2954
+    scoreboard = sorted(scoreboard, key=lambda x: x['score'], reverse=True)
 
     return jsonify(scoreboard=scoreboard)
 
