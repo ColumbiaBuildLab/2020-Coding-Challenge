aa5aa85da1ebdfb1107331783558e8f26c65cf91
hanvitC <121269386+hanvitC@users.noreply.github.com>
2022-12-22 20:08:24 -0500

    modifying scoreboard.js

---
+++ scoreboard.js
@@ -0,0 +1,57 @@
+function display_scoreboard(scoreboard){
+  $("#teams").empty();
+
+  scoreboard.sort((a, b) => b.score - a.score);
+
+  $.each(scoreboard, function(index, team){
+    console.log(team.name);
+    addTeamView(team.id, team.name, team.score);
+  });
+
+}
+
+function compareCount(a, b){
+  return a.score - b.score;
+}
+
+function addTeamView(id, name, score){
+  var team_template = $("<div class = row></div>");
+  var name_template = $("<div class = col-md-5></div>");
+  var score_template = $("<div class = col-md-2></div>");
+  var button_template = $("<div class = col-md-2></div>");
+  var increase_button = $("<button class = increase-button>+</button>");
+  $(increase_button).click(function(){
+    increase_score(id);
+  });
+  name_template.text(name);
+  score_template.text(score);
+  button_template.append(increase_button);
+  team_template.append(name_template);
+  team_template.append(score_template);
+  team_template.append(button_template);
+  $("#teams").append(team_template);
+}
+
+function increase_score(id){
+  var team_id = {"id": id}
+  $.ajax({
+    type: "POST",
+    url: "increase_score",                
+    dataType : "json",
+    contentType: "application/json; charset=utf-8",
+    data : JSON.stringify(team_id),
+    success: function(result){
+      window.location.href = 'http://127.0.0.1:5000/';
+    },
+    error: function(request, status, error){
+        console.log("Error");
+        console.log(request)
+        console.log(status)
+        console.log(error)
+    }
+  });
+}
+
+$(document).ready(function(){
+  display_scoreboard(scoreboard);
+})
