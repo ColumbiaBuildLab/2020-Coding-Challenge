b7b611fe99de80aca0a093dfc71b5db2d5762fc7
DongShengye <bestshengye@gmail.com>
2023-01-20 00:47:06 -0500

    Coding Challenge-Shengye Dong-sd3599
    Coding Challenge-Shengye Dong-sd3599

--- server.py
+++ server.py
@@ -1,52 +1,57 @@
 from flask import Flask
 from flask import render_template
 from flask import Response, request, jsonify
+from copy import deepcopy
 app = Flask(__name__)
 
 scoreboard = [
     {
-    "id": 1,
-    "name": "Boston Bruins",
-    "score": 7
+        "id": 1,
+        "name": "Boston Bruins",
+        "score": 7
     },
 
     {
-    "id": 2,
-    "name": "Tampa Bay Lightning", 
-    "score": 5
+        "id": 2,
+        "name": "Tampa Bay Lightning",
+        "score": 5
     },
 
     {
-    "id": 3,
-    "name": "Toronto Maple Leafs", 
-    "score": 2
+        "id": 3,
+        "name": "Toronto Maple Leafs",
+        "score": 2
     },
 
     {
-    "id": 4,
-    "name": "Florida Panthers", 
-    "score": 1
+        "id": 4,
+        "name": "Florida Panthers",
+        "score": 1
     },
 
     {
-    "id": 5,
-    "name": "Buffalo Sabres", 
-    "score": 1
+        "id": 5,
+        "name": "Buffalo Sabres",
+        "score": 1
     },
 ]
 
 
 @app.route('/')
 def show_scoreboard():
-    return render_template('scoreboard.html', scoreboard = scoreboard) 
+    
+    global scoreboard
+    scoreboard = sorted(scoreboard, key=lambda x: x["score"], reverse=True)
+    return render_template('scoreboard.html', scoreboard=scoreboard)
+
 
 @app.route('/increase_score', methods=['GET', 'POST'])
 def increase_score():
     global scoreboard
 
-    json_data = request.get_json()   
-    team_id = json_data["id"]  
-    
+    json_data = request.get_json()
+    team_id = json_data["id"]
+    print(json_data)
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
@@ -55,8 +60,4 @@ def increase_score():
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
-
-
-
-
+    app.run(debug=True)
--- static/scoreboard.js
+++ static/scoreboard.js
@@ -1,9 +1,14 @@
 function display_scoreboard(scoreboard){
+  
+
+
   $("#teams").empty();
+  
   $.each(scoreboard, function(index, team){
     addTeamView(team.id, team.name, team.score);
   });
 }
+
 
 function addTeamView(id, name, score){
   var team_template = $("<div class = row></div>");
@@ -12,8 +17,34 @@ function addTeamView(id, name, score){
   var button_template = $("<div class = col-md-2></div>");
   var increase_button = $("<button class = increase-button>+</button>");
   $(increase_button).click(function(){
+    var i;
+    score = score+1
+    score_template.text(score);
     increase_score(id);
+
+    scoreboard.sort(function(a, b) {
+      return parseFloat(b.score) - parseFloat(a.score);
   });
+
+    for (i = 0; i < scoreboard.length; i++) {
+      if (scoreboard[i].id == id){
+        scoreboard[i].score +=1;
+        var buffer = i;
+      }
+    }
+    if ((buffer) != 0){
+      console.log(scoreboard[buffer].score)
+      console.log(scoreboard[buffer-1].score)
+      if (scoreboard[buffer].score>scoreboard[buffer-1].score){
+        location.reload(true);
+      }
+    }
+
+
+
+  });
+
+
   name_template.text(name);
   score_template.text(score);
   button_template.append(increase_button);
@@ -21,6 +52,7 @@ function addTeamView(id, name, score){
   team_template.append(score_template);
   team_template.append(button_template);
   $("#teams").append(team_template);
+  console.log(team_template)
 }
 
 function increase_score(id){
@@ -32,7 +64,7 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+
     },
     error: function(request, status, error){
         console.log("Error");
@@ -42,6 +74,8 @@ function increase_score(id){
     }
   });
 }
+
+
 
 $(document).ready(function(){
   display_scoreboard(scoreboard);
