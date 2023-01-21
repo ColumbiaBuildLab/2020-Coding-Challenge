8bb5fa61fde48c9eeaadf94bb23c08c67b82cf38
ayshajamjam <aj2604@columbia.edu>
2023-01-20 20:01:45 -0500

    Completed the challenge

--- server.py
+++ server.py
@@ -51,6 +51,9 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    # Part 2
+    scoreboard.sort(key=lambda x: x['score'], reverse=True)
+
     return jsonify(scoreboard=scoreboard)
 
 
--- static/scoreboard.js
+++ static/scoreboard.js
@@ -32,7 +32,8 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+      // Part 1
+      display_scoreboard(result.scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
