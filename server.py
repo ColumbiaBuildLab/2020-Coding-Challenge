72b04fd4cc65847d0ccf595f6543f245b619c8db
JetLi-99 <jetljt99@gmail.com>
2023-04-25 22:40:03 -0400

    done. Jingtao Li jl6098

--- server.py
+++ server.py
@@ -51,6 +51,8 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    scoreboard.sort(key=lambda x: (-x["score"]))
+   
     return jsonify(scoreboard=scoreboard)
 
 
