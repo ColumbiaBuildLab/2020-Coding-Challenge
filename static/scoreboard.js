a2c6e58dbebb9e12528ff77778e804b53bb33283
Shreeya Patel <shreeyapaatel@gmail.com>
2023-04-29 22:25:49 -0400

    Attempted my best shot at rendering scores and sorting

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -1,5 +1,8 @@
 function display_scoreboard(scoreboard){
   $("#teams").empty();
+  //sort in decreasing order
+  scoreboard.sort(function(team_a, team_b) {return team_b.score - team_a.score});
+
   $.each(scoreboard, function(index, team){
     addTeamView(team.id, team.name, team.score);
   });
@@ -12,7 +15,7 @@ function addTeamView(id, name, score){
   var button_template = $("<div class = col-md-2></div>");
   var increase_button = $("<button class = increase-button>+</button>");
   $(increase_button).click(function(){
-    increase_score(id);
+    increase_score(id, score_template);
   });
   name_template.text(name);
   score_template.text(score);
@@ -23,7 +26,8 @@ function addTeamView(id, name, score){
   $("#teams").append(team_template);
 }
 
-function increase_score(id){
+// Increase score function
+function increase_score(id, score_template){
   var team_id = {"id": id}
   $.ajax({
     type: "POST",
@@ -32,7 +36,16 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
+        // here
+        // update score for team
+        $('#teams').empty();
+        var scoreboard_res = result.scoreboard;
+        scoreboard_res.sort(function(a, b){return b.score - a.score});
+        for (var i = 0; i < scoreboard_res.length; i++) {
+          addTeamView(scoreboard_res[i].id, scoreboard_res[i].name, scoreboard_res[i].score);
+        }
         
+        display_scoreboard(result.scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
