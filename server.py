34d9289d1c55db44172bca6be9fca80cbd3e49ce
Hangpu Cao <114501286+HangpuCao@users.noreply.github.com>
2023-05-03 16:39:08 -0400

    Update server.py

--- server.py
+++ server.py
@@ -50,6 +50,8 @@ def increase_score():
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
+            
+    scoreboard = sorted(scoreboard, key=lambda score: score['score'], reverse=True)     # sort the teams
 
     return jsonify(scoreboard=scoreboard)
 
