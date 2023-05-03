19361b41c3eeae329b2e396f93c7b1242556a650
Emilie Xu <31358826+emilie-xu@users.noreply.github.com>
2023-05-03 00:06:01 -0400

    display and sort scoreboard

--- server.py
+++ server.py
@@ -50,6 +50,9 @@ def increase_score():
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
+            
+    #sort scoreboard top to bottom; no need for alphabetical sorting
+    scoreboard.sort(key=lambda team: team['score'], reverse=True)
 
     return jsonify(scoreboard=scoreboard)
 
