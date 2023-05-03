e681fb3473147459a3e2983c01875e982305a5df
seongholee4 <leese@dickinson.edu>
2023-05-03 18:21:49 +0900

    Seongho Lee, SL5036: Sort the scoreboard in non-increasing order of scores and reflect a score change immediately on the front-end without refreshing the page.

--- server.py
+++ server.py
@@ -35,7 +35,16 @@ scoreboard = [
     },
 ]
 
-
+'''
+1) The goal is to reflect this change immediately on the front-end. 
+2) Once that's done, we would also like to sort the teams 
+- so that whenever there's a score change, the list would change 
+- so that the teams are listed in non-increasing order of scores from top to bottom
+(you don't need to sort them alphabetically when there is a tie). 
+To do these, you would need to make changes in both server.py file and scoreboard.js file. 
+When you're done, please send a pull request to this repository with your name and uni in the comment. 
+Thanks and good luck!
+'''
 @app.route('/')
 def show_scoreboard():
     return render_template('scoreboard.html', scoreboard = scoreboard) 
@@ -51,12 +60,11 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    # Sort the scoreboard in non-increasing order of scores
+    scoreboard = sorted(scoreboard, key=lambda x: x['score'], reverse=True)
+
     return jsonify(scoreboard=scoreboard)
 
 
 if __name__ == '__main__':
    app.run(debug = True)
-
-
-
-
