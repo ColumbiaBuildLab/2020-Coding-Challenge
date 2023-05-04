74ccf5b3bfb342dd262e08ff8ccfc0070f8f2777
Jianyang Duan <jd3794@columbia.edu>
2023-05-03 20:49:43 -0400

    jd3794 Jennifer Duan

--- server.py
+++ server.py
@@ -51,6 +51,8 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    scoreboard = sorted(scoreboard, key=lambda x: x["score"], reverse=True)
+
     return jsonify(scoreboard=scoreboard)
 
 
