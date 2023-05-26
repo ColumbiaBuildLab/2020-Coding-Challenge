e2b0e28ef78c2e35e25f95a18a9bcf5fe72f0fe8
Kashish Goel <kashijioel930@gmail.com>
2023-05-26 00:25:02 -0400

    live scoreboard changes (CBL)

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -32,7 +32,8 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+      // Update the scoreboard with the updated scores
+      display_scoreboard(result.scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
