0a8eb11c96e912a7e14f4347ff733dfa1d1f0509
ellayunjikim <kimella@grinnell.edu>
2023-08-03 19:35:19 -0400

    Changed code

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -1,5 +1,11 @@
 function display_scoreboard(scoreboard){
+  // Sort the array by non-increasing order 
+  scoreboard.sort(function(a,b) {
+    return b.score - a.score;
+  });  
+
   $("#teams").empty();
+
   $.each(scoreboard, function(index, team){
     addTeamView(team.id, team.name, team.score);
   });
@@ -9,8 +15,8 @@ function addTeamView(id, name, score){
   var team_template = $("<div class = row></div>");
   var name_template = $("<div class = col-md-5></div>");
   var score_template = $("<div class = col-md-2></div>");
-  var button_template = $("<div class = col-md-2></div>");
-  var increase_button = $("<button class = increase-button>+</button>");
+  var button_template = $("<div class = col-md-3></div>");
+  var increase_button = $("<button class='increase-button' data-id='" + id + "'>+</button>");
   $(increase_button).click(function(){
     increase_score(id);
   });
@@ -32,7 +38,13 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+      // Update the score on the correct element and location
+      var newScore = result.scoreboard.find(team => team.id === id).score;
+      var scoreElem = $(`[data-id="${id}"]`).closest('.row').find('.col-md-2');
+      scoreElem.text(newScore);
+
+      // Sort the scoreboard dynamically by recalling the display every time there is increase in score 
+      display_scoreboard(result.scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
@@ -44,5 +56,6 @@ function increase_score(id){
 }
 
 $(document).ready(function(){
+  // Display the initial scoreboard
   display_scoreboard(scoreboard);
-})
+});
