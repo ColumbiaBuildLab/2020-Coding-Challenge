f481e79cce3e46c513ac2c95960ec13e402201aa
Andrew Stein <aps2231@columbia.edu>
2023-08-09 14:39:08 -0400

    End result hopefully

--- server.py
+++ server.py
@@ -55,8 +55,9 @@ def increase_score():
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
-
-    return jsonify(scoreboard=scoreboard)
+    
+    sorted_scoreboard = sorted(scoreboard, key=lambda x: (-x['score'], x['name']))
+    return jsonify(scoreboard=sorted_scoreboard)
 
 
 if __name__ == '__main__':
