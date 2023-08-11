51b2821de3108cec2979880fd91b2c44454f4084
Sumya <sumyahoque2012@gmail.com>
2023-08-11 16:30:36 -0400

    changes maded

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -1,3 +1,4 @@
+
 function display_scoreboard(scoreboard){
   $("#teams").empty();
   $.each(scoreboard, function(index, team){
@@ -32,8 +33,8 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
-    },
+      display_scoreboard(result.scoreboard)
+     },
     error: function(request, status, error){
         console.log("Error");
         console.log(request)
