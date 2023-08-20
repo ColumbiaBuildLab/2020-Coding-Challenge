76780c8fdf0e995d5ef4d0db835e8e07258d1d87
Carly Kiang <carlykiang@carlys-air-2.lan>
2023-08-20 13:23:04 +0800

    Carly Kiang tk2990

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import Response, request, jsonify
+import operator
 app = Flask(__name__)
 
 scoreboard = [
@@ -38,9 +39,10 @@ scoreboard = [
 
 @app.route('/')
 def show_scoreboard():
+    jsonify(scoreboard=scoreboard)
     return render_template('scoreboard.html', scoreboard = scoreboard) 
 
-@app.route('/increase_score', methods=['GET', 'POST'])
+@app.route('/increase_score', methods=['POST'])
 def increase_score():
     global scoreboard
 
@@ -50,7 +52,7 @@ def increase_score():
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
-
+    scoreboard = sorted(scoreboard, key=operator.itemgetter('score'), reverse=True)
     return jsonify(scoreboard=scoreboard)
 
 
