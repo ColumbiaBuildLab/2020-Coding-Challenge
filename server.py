2c3fb3e2648a921a01ae5f6f2eb008bde3863c5f
yemily17 <46946140+yemily17@users.noreply.github.com>
2023-08-23 00:25:38 -0400

    Emily Yin ecy2119

--- server.py
+++ server.py
@@ -50,12 +50,9 @@ def increase_score():
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
-        scoreboard = sorted(scoreboard, reverse = True, key = lambda i: i['score'])
+            scoreboard=sorted(scoreboard, key=lambda k: k['score'], reverse=True)
     return jsonify(scoreboard=scoreboard)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
-
-
-
-
