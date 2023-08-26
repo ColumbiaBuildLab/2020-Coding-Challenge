a1742de8430e988cec429e064c0809f959762942
Nicholas Djedjos <72480037+reformist@users.noreply.github.com>
2023-08-26 17:00:08 -0400

    Update scoreboard.js

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -1,7 +1,14 @@
+/*
+Nicholas Djedjos
+08/26/2023
+CBL Coding Challenge
+*/
+
 function display_scoreboard(scoreboard){
   $("#teams").empty();
   $.each(scoreboard, function(index, team){
     addTeamView(team.id, team.name, team.score);
+    console.log("does this work");
   });
 }
 
@@ -11,9 +18,13 @@ function addTeamView(id, name, score){
   var score_template = $("<div class = col-md-2></div>");
   var button_template = $("<div class = col-md-2></div>");
   var increase_button = $("<button class = increase-button>+</button>");
+
+  // $ symbolizes a jQuery object
+
   $(increase_button).click(function(){
     increase_score(id);
   });
+
   name_template.text(name);
   score_template.text(score);
   button_template.append(increase_button);
@@ -22,17 +33,22 @@ function addTeamView(id, name, score){
   team_template.append(button_template);
   $("#teams").append(team_template);
 }
-
 function increase_score(id){
   var team_id = {"id": id}
-  $.ajax({
-    type: "POST",
+  /* .ajax allows web pages to be updated asynchronously*/
+  $.getJSON({
+    type: "POST", 
     url: "increase_score",                
     dataType : "json",
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
-    success: function(result){
-        
+
+   
+    success: function(result){ /* result holds the data the server has sent back in response to request*/
+      // problem was result has the correct value but isn't the same data type as old scoreboard
+      scoreboard = result.scoreboard
+      display_scoreboard(scoreboard);
+
     },
     error: function(request, status, error){
         console.log("Error");
@@ -42,6 +58,8 @@ function increase_score(id){
     }
   });
 }
+
+// after document is loaded, this happens
 
 $(document).ready(function(){
   display_scoreboard(scoreboard);
