78e44e6a7c71c3139a9fb639e84847bef81c3e6b
sandrazelen <sz3120@columbia.edu>
2023-12-02 18:19:52 -0500

    added sorting functionality

--- server.py
+++ server.py
@@ -51,6 +51,8 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    scoreboard = sorted(scoreboard, key = lambda i: i["score"], reverse = True)
+
     return jsonify(scoreboard=scoreboard)
 
 
