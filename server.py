6dcceade6038812e3edb26230fb49d77731b0343
YeongWoo (Janie) Kim <107515314+janieyw@users.noreply.github.com>
2023-12-03 18:01:25 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,52 +1,30 @@
 from flask import Flask
 from flask import render_template
 from flask import Response, request, jsonify
+
 app = Flask(__name__)
 
 scoreboard = [
-    {
-    "id": 1,
-    "name": "Boston Bruins",
-    "score": 7
-    },
-
-    {
-    "id": 2,
-    "name": "Tampa Bay Lightning", 
-    "score": 5
-    },
-
-    {
-    "id": 3,
-    "name": "Toronto Maple Leafs", 
-    "score": 2
-    },
-
-    {
-    "id": 4,
-    "name": "Florida Panthers", 
-    "score": 1
-    },
-
-    {
-    "id": 5,
-    "name": "Buffalo Sabres", 
-    "score": 1
-    },
+    {"id": 1, "name": "Boston Bruins", "score": 7},
+    {"id": 2, "name": "Tampa Bay Lightning", "score": 5},
+    {"id": 3, "name": "Toronto Maple Leafs", "score": 2},
+    {"id": 4, "name": "Florida Panthers", "score": 1},
+    {"id": 5, "name": "Buffalo Sabres", "score": 1},
 ]
 
 
-@app.route('/')
+@app.route("/")
 def show_scoreboard():
-    return render_template('scoreboard.html', scoreboard = scoreboard) 
+    return render_template("scoreboard.html", scoreboard=scoreboard)
 
-@app.route('/increase_score', methods=['GET', 'POST'])
+
+@app.route("/increase_score", methods=["GET", "POST"])
 def increase_score():
     global scoreboard
 
-    json_data = request.get_json()   
-    team_id = json_data["id"]  
-    
+    json_data = request.get_json()
+    team_id = json_data["id"]
+
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
@@ -54,9 +32,5 @@ def increase_score():
     return jsonify(scoreboard=scoreboard)
 
 
-if __name__ == '__main__':
-   app.run(debug = True)
-
-
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
