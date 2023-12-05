595af59330b59f59eb8d5c14e156a1d294dfb1c9
maoy0125 <142650757+maoy0125@users.noreply.github.com>
2023-12-05 16:39:08 -0500

    Update scoreboard.js

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -32,7 +32,7 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+        display_scoreboard(result.scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
