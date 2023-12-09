c33d17318cb76dd8158fb07e6e8f05cd4d4cd2a8
Sophie Agbekpenou <sophiexabla@gmail.com>
2023-12-08 20:10:28 -0500

    CLB-SPRING-2024

--- server.py
+++ server.py
@@ -38,7 +38,7 @@ scoreboard = [
 
 @app.route('/')
 def show_scoreboard():
-    return render_template('scoreboard.html', scoreboard = scoreboard) 
+    return render_template('scoreboard.html', scoreboard = sorted(scoreboard, key=lambda x: x["score"], reverse=True)) 
 
 @app.route('/increase_score', methods=['GET', 'POST'])
 def increase_score():
@@ -50,12 +50,14 @@ def increase_score():
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
-
+    
+    scoreboard = sorted(scoreboard, key=lambda x: x["score"], reverse=True) 
+   
     return jsonify(scoreboard=scoreboard)
 
-
+  
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(debug = True, port = 5001)
 
 
 
