393a3abcfab54a317a33655dd4eb60d2d7ef7345
charlhhhh <clha826@gmail.com>
2023-12-12 19:43:21 -0500

    Task finished

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -27,18 +27,19 @@ function increase_score(id){
   var team_id = {"id": id}
   $.ajax({
     type: "POST",
-    url: "increase_score",                
+    url: "increase_score",
     dataType : "json",
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+      scoreboard = result.scoreboard;
+      display_scoreboard(scoreboard);
     },
     error: function(request, status, error){
-        console.log("Error");
-        console.log(request)
-        console.log(status)
-        console.log(error)
+      console.log("Error");
+      console.log(request)
+      console.log(status)
+      console.log(error)
     }
   });
 }
