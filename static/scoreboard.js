f858b71ce177e31d5308c2fef3e671c0813ac51a
liucharlie213 <liu.charlie213@gmail.com>
2023-12-14 15:28:55 -0500

    completed challenge

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -1,5 +1,9 @@
 function display_scoreboard(scoreboard){
   $("#teams").empty();
+  // sort scoreboard before displaying
+  scoreboard = scoreboard.sort(
+    (t1, t2) => (t1.score < t2.score) ? 1 : (t1.score > t2.score ? -1 : 0)
+  ) 
   $.each(scoreboard, function(index, team){
     addTeamView(team.id, team.name, team.score);
   });
@@ -32,7 +36,8 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+      // display updated scoreboard right away
+      display_scoreboard(result.scoreboard)
     },
     error: function(request, status, error){
         console.log("Error");
