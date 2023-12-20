9416a39bc63fae788c5031703f168b57892cba99
maoo1 <madeline.ou@gmail.com>
2023-12-20 17:52:13 -0600

    added sort ln 54

--- server.py
+++ server.py
@@ -51,7 +51,7 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
-    # scoreboard.sort(key=lambda x: x["score"], reverse=True)
+    scoreboard.sort(key=lambda x: x["score"], reverse=True)
 
     return jsonify(scoreboard=scoreboard)
 
